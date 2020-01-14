11f491beeb1df69870c989fe1f84336e9aef1acf
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-01-14 21:38:55 +0100

    feat: Add agent worker

---
+++ pkg/workers/agent.go
@@ -0,0 +1,41 @@
+package workers
+
+import (
+	"github.com/pojntfx/go-isc-dhcp/pkg/utils"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// K3SAgent is the k3s agent.
+type K3SAgent struct {
+	utils.ProcessWorker
+	NetworkDevice string
+	Token         string
+	ServerURL     string
+	BinaryDir     string
+}
+
+// Start starts the the k3s agent.
+func (a *K3SAgent) Start() error {
+	a.ScheduledForDeletion = false
+
+	command := exec.Command(
+		a.BinaryDir,
+		"agent",
+		"--flannel-interface",
+		a.NetworkDevice,
+		"--token",
+		a.Token,
+		"--server",
+		a.ServerURL)
+
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	command.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	a.Instance = command
+
+	return command.Start()
+}
