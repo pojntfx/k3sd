6080ecc710e5ef217bc6be9597a7fe573fdb75ae
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-01-20 22:52:09 +0100

    feat: Add `start` command to agent CLI (#10)

---
+++ cmd/k3sagentctl/cmd/constants.go
@@ -0,0 +1,11 @@
+package cmd
+
+const (
+	keyPrefix         = "k3sagent."
+	configFileDefault = ""
+	serverHostPortKey = keyPrefix + "serverHostPort"
+	configFileKey     = keyPrefix + "configFile"
+	networkDeviceKey  = keyPrefix + "networkDevice"
+	tokenKey          = keyPrefix + "token"
+	serverUrlKey      = keyPrefix + "serverUrl"
+)
