828da84bb594bb9b573da98a90fbb4fecae04422
Felix Pojtinger <felix@pojtinger.com>
2020-01-20 20:44:44 +0100

    refactor: Enable usage of cleanup etc. functionality without starting server/agent (#9)

--- pkg/svc/constants.go
+++ pkg/svc/constants.go
@@ -3,6 +3,8 @@ package svc
 import "path/filepath"
 
 var (
-	DataPath   = filepath.Join("/var", "lib", "rancher", "k3s") // DataPath is the path to where the k3s data is being stored.
-	ConfigPath = filepath.Join("/etc", "rancher", "k3s")        // ConfigPath is the path in which the k3s configuration resides.
+	DataPath       = filepath.Join("/var", "lib", "rancher", "k3s") // DataPath is the path to where the k3s data is being stored.
+	ConfigPath     = filepath.Join("/etc", "rancher", "k3s")        // ConfigPath is the path in which the k3s configuration resides.
+	KubeconfigPath = filepath.Join(ConfigPath, "k3s.yaml")          // KubeconfigPath is the path to the kubeconfig file.
+	TokenPath      = filepath.Join(DataPath, "server", "token")     // TokenPath is the path to the k3s token.
 )
