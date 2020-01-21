571237c2e132f8810955ceebb188b26baa871b25
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-01-21 08:15:13 +0100

    feat: Add `get kubeconfig` command to server CLI (#11)

---
+++ cmd/k3sserverctl/cmd/get-kubeconfig.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	constants "github.com/pojntfx/k3sd/cmd"
+	k3sd "github.com/pojntfx/k3sd/pkg/proto/generated"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gitlab.com/bloom42/libs/rz-go"
+	"gitlab.com/bloom42/libs/rz-go/log"
+	"google.golang.org/grpc"
+)
+
+var getKubeconfigCmd = &cobra.Command{
+	Use:     "kubeconfig",
+	Aliases: []string{"k"},
+	Short:   "Get a kubeconfig",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if !(viper.GetString(configFileKey) == configFileDefault) {
+			viper.SetConfigFile(viper.GetString(configFileKey))
+
+			if err := viper.ReadInConfig(); err != nil {
+				return err
+			}
+		}
+
+		conn, err := grpc.Dial(viper.GetString(serverHostPortKey), grpc.WithInsecure(), grpc.WithBlock())
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+
+		client := k3sd.NewK3SServerManagerClient(conn)
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		kubeconfig, err := client.GetKubeconfig(ctx, &k3sd.K3SServerEmptyArgs{})
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(kubeconfig.GetKubeconfig())
+
+		return nil
+	},
+}
+
+func init() {
+	var (
+		serverHostPortFlag string
+		configFileFlag     string
+	)
+
+	getKubeconfigCmd.PersistentFlags().StringVarP(&serverHostPortFlag, serverHostPortKey, "s", constants.K3SDHostPortDefault, "Host:port of the k3sd server to use.")
+	getKubeconfigCmd.PersistentFlags().StringVarP(&configFileFlag, configFileKey, "f", configFileDefault, "Configuration file to use.")
+
+	if err := viper.BindPFlags(getKubeconfigCmd.PersistentFlags()); err != nil {
+		log.Fatal(constants.CouldNotBindFlagsErrorMessage, rz.Err(err))
+	}
+
+	viper.AutomaticEnv()
+
+	getCmd.AddCommand(getKubeconfigCmd)
+}
